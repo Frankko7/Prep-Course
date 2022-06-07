ec968fa1fc0fc689c4260a9784b82897686d6022
frankko7 <francodiaztamayo@gmail.com>
2022-06-07 11:42:43 -0300

    homeworkjsII

---
+++ 03-JS-II/homework/ExplicacionesJS2.js
@@ -0,0 +1,14 @@
+"Bucle for"
+
+const { doesNotMatch } = require("assert")
+
+el bucle  for ayuda a repetir un bloque de codigo cuantas veces deseemos.
+en el bucle for se deben declarar contador de cantidad de repeticiones del bucle, condicion de frenado y 
+paso, es decir como quiero que se incremenete el valor.
+ejemplo: for (var 1 =0 (contador); i<= 20(frenado);i++(paso)){bloque de codigo}
+
+"operadores logicos"
+comparan dos expresiones de igualdad
+&& (and) --> devuelve true solo cuando ambas variables son true 
+|| (OR)  --> devuelve false solo cuando ambas variables son falsas
+! (not) --> negacion. devuelve el valor booleano opuesto al que recive 
