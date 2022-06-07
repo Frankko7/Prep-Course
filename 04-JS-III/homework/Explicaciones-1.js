e74c5c0bc4880b612b8e356a815f1ecbb4151e6a
frankko7 <francodiaztamayo@gmail.com>
2022-06-07 20:44:56 -0300

    homeworkIII.js

---
+++ 04-JS-III/homework/Explicaciones-1.js
@@ -0,0 +1,6 @@
+"ARRAYS"
+Un array es una lista de elementos ordenados a los cuales se puede acceder.
+los array pueden recibir todo tipo de datos, funciones e incluso otros arrays.
+se definen usando corchetes. Ej: var array = [];
+pueden estar vacios o tener elementos 
+para acceder a los elementos dentro del array se debe llamar a su posicion dentro del array (el primero es 0, el segundo 1 y asi sucesivamente)
